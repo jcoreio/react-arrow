b2a5cf3d01664484912422a696f905df745f6f11
Andy Edwards <jedwards@fastmail.com>
2023-11-13 19:45:23 -0600

    fix: add SVG props to arrow props

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,15 @@
 import * as React from 'react'
 
-type Props = {
-  shaftWidth: number
-  shaftLength: number
-  headWidth: number
-  headLength: number
-  direction: 'left' | 'right' | 'up' | 'down' | 'leftRight' | 'upDown'
-  unclosed?: boolean
-  transform?: string
-}
+type Props = React.ComponentProps<'svg'> &
+  React.ComponentProps<'path'> & {
+    shaftWidth: number
+    shaftLength: number
+    headWidth: number
+    headLength: number
+    direction: 'left' | 'right' | 'up' | 'down' | 'leftRight' | 'upDown'
+    unclosed?: boolean
+    transform?: string
+  }
 
 declare const Arrow: React.ComponentType<Props>
 export default Arrow
